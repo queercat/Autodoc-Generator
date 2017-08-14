63b5cc84fbf08bd9b003427c96b95e19be46a6b3
GeekishGirl <tusek.nicole@gmail.com>
2017-08-13 18:47:54 -0700

    Began the tokenization. Needs some major work.

--- autodoc.js
+++ autodoc.js
@@ -4,8 +4,8 @@ const fs = require('file-system');
 
 /* Global Vars */
 const encodingType = "UTF-8"
-
-var lexemes = ["/**", "*/", "*", "@", "//"]; //List of special combination of words to look for.
+var functions = []; //The functions and their descriptors.
+var descriptors = [];
 
 if (args <= 0 || args.indexOf("help") != -1 || args.indexOf("--help") != -1) { //Checking to make sure that the arguments specified are valid.
 	displayUsage();
@@ -29,24 +29,76 @@ function readfile(filename, encoding) {
 
 /**
  * @description Process the data of an assumed file stream.
- * @param  {string} data the data being processed
+ * @param {string} data the data being processed.
  */
 function processData(data) {
-	while (data.indexOf(lexemes[0]) !== -1) { //While there still exists doc comments.
-		var commentStart = data.indexOf(lexemes[0]); //Get the start of the comment.
-		var commentEnd = data.indexOf(lexemes[1]) + 2; //Get the end of the comment.
+
+	data = removeComments(data); //Remove the comments from the program.
+
+	while (data.indexOf("/**") !== -1) { //While there still exists doc comments.
+		var commentStart = data.indexOf("/**"); //Get the start of the comment.
+		var commentEnd = data.indexOf("*/") + 2; //Get the end of the comment.
 
 		var doc = data.slice(commentStart + 3, commentEnd - 2); //Create the documentation block and remove the first /** structure and the last / structure.
 
-		while (doc != "") {
-			console.log(1);
+		var lines = [];
+
+		while (doc) {
+			lexPos = doc.indexOf("*");
+			var line = doc.slice(lexPos + 1, doc.indexOf("*", lexPos + 1)); //The line is is equal to the substr of the doc where * starts and the next * stars.
+			lines.push(line.trim());
+			doc = doc.slice(line.length + 1, doc.length);
 		}
 
-		data = data.slice(commentEnd + 2, data.length); //Remove the comment so we don't comment it again.
-	}
+		lines.pop(); //Remove last element as for some reason weird issue with copying.
 
-	console.log(data);
+		var name; //Name of the function.
+
+		data = data.slice(commentEnd + 2, data.length); //Remove the comment so we don't parse it again.
+
+		var functionStart = 0; //Get the start of where the function is.
+		var functionEnd = data.indexOf("{"); //Get the end of where the function is.
+
+		var functionLine = data.slice(functionStart, functionEnd).trim();
+
+		if (functionLine.indexOf("=") !== -1) {
+			name = functionLine.slice(0, functionLine.indexOf("=") - 1).trim();
+		} else {
+			name = functionLine.slice(functionLine.indexOf("function") + "function".length, functionLine.indexOf("(")).trim();
+		}
+
+		var data = data.slice(data.indexOf("/**"), data.length);
+
+		functions.push(name); //Push the names of the functions to the function array.
+		descriptors.push(lines); //Push the descriptors to the descriptors array.
+
+		createDocumentation();
+	}
 }
+
+/**
+ * Remove all comments from the input data.
+ * @param {string} data the data being input.
+ * @return {string} the data with the comments removed. 
+ */
+function removeComments(data) {
+		var regEx = /(\/\/)|(\/\*[^*].*)|(\/\*\*\*.*).*|/g //Reg ex to remove comments. such as /* */ or // or even /********************************** */ just not /**
+		var regEx2 = /{([^}]+)}/g
+		data = data.replace(regEx, ''); //Remove all comments.
+		data = data.replace(regEx2, ''); //Remove all things inside braces.
+
+		console.log(data);
+
+		return data;
+}
+
+/**
+ * Create the documentation.
+ */
+function createDocumentation() {
+	console.log(functions);
+	console.log(descriptors);
+}		
 
 /**
  * @description Display usage information about the program.
@@ -63,6 +115,7 @@ function failExit(why) {
 	if (why != null) {
 		console.log(why);
 	}
+
 	process.exit(-1);
 }
 
--- test.js
+++ test.js
@@ -6,6 +6,32 @@ requires('file-system');
  * @param  {int} test test test testing testiest test tets line2
  * @return {string} teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeest line3
  */
+
 function test(testing, test) {
+	return test
+}
+
+// * *// *// /* /** *// /* */ /* *;/ /* ... _ functio n*/// /* 8? &&& */
+// * *// *// /* /** *// /* */ /* *;/ /* ... _ functio n*/// /* 8? &&& */
+// 
+// 
+// 
+// // * *// *// /* /** *// /* */ /* *;/ /* ... _ functio n*/// /* 8? &&& */
+// 
+/// * *// *// /* /** *// /* */ /* *;/ /* ... _ functio n*/// /* 8? &&& */
+// * *// *// /* /** *// /* */ /* *;/ /* ... _ functio n*/// /* 8? &&& */
+// 
+
+
+
+// * *// *// /* /** *// /* */ /* *;/ /* ... _ functio n*/// /* 8? &&& */
+/**
+ * @description testing testy test tester mctest the testing the III test line0
+ * @param  {string} testing tester for the testing testiest test test test... test line1
+ * @param  {int} test test test testing testiest test tets line2
+ * @return {string} teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeest line3
+ */
+
+test = function(testing, test) {
 	return test
 }
